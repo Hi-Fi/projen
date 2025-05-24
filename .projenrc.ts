66fb2868e873419bc593d5f7953bab7423837fb8
Juho Saarinen <juho.saarinen@gmail.com>
2025-05-24 20:02:51 +0300

    feat: include Biome support to projen. Fixes ##3966
    Allows to use [Biome](https://biomejs.dev/) for linting and formatting.

--- .projenrc.ts
+++ .projenrc.ts
@@ -83,6 +83,10 @@ const project = new JsiiProject({
     "markmac",
     "esbuild",
     "all-contributors-cli",
+    "json-schema-to-typescript",
+    // Can be removed if linting and formating is done with Biome
+    "@biomejs/biome",
+
   ],
 
   peerDeps: ["constructs@^10.0.0"],
